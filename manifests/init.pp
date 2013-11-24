5fc18e4b8f0b4203acf11300fd6d458bb71811b0
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-11-24 19:46:04 +0900

    Add manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Installs Bowtie
+#
+# Examples:
+#
+#    include aquaskk
+
+class aquaskk {
+  package { 'aquaskk':
+    provider => 'pkgdmg',
+    source   => 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Faquaskk%2F58634%2FAquaSKK-20130421.dmg'
+  }
 }
