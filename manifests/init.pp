1fa32b8ad3bdba18923aab5561f9840a4fed2874
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-11-24 19:50:34 +0900

    Update source url

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Public: Installs Bowtie
+# Public: Installs AquaSKK
 #
 # Examples:
 #
@@ -6,7 +6,7 @@
 
 class aquaskk {
   package { 'aquaskk':
-    provider => 'pkgdmg',
-    source   => 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Faquaskk%2F58634%2FAquaSKK-20130421.dmg'
+    source   => 'http://jaist.dl.sourceforge.jp/aquaskk/58634/AquaSKK-20130421.dmg',
+    provider => 'pkgdmg'
   }
 }
